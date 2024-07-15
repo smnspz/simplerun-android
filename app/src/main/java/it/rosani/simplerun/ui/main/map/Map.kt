9a0591904cc218e5929e8aa2f39ee3e75641814f
Simone Rosani <rsn.smn@gmail.com>
2024-07-15 14:54:21 +0200

    chore: refactor Map into its own file

---
+++ app/src/main/java/it/rosani/simplerun/ui/main/map/Map.kt
@@ -0,0 +1,37 @@
+package it.rosani.simplerun.ui.main.map
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import com.utsman.osmandcompose.MapProperties
+import com.utsman.osmandcompose.OpenStreetMap
+import com.utsman.osmandcompose.ZoomButtonVisibility
+import com.utsman.osmandcompose.rememberCameraState
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+
+@Composable
+fun Map(modifier: Modifier = Modifier) {
+    var mapProperties by remember { mutableStateOf(MapProperties()) }
+
+    SideEffect {
+        mapProperties = mapProperties
+            .copy(zoomButtonVisibility = ZoomButtonVisibility.NEVER)
+            .copy(tileSources = TileSourceFactory.MAPNIK)
+    }
+
+    val cameraState = rememberCameraState {
+        geoPoint = GeoPoint(51.5074, -0.1278)
+        zoom = 19.9
+    }
+
+    OpenStreetMap(
+        modifier = modifier,
+        cameraState = cameraState,
+        properties = mapProperties,
+    )
+}
