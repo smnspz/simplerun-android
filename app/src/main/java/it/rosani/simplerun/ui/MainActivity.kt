dade0c7bc6384a293ef5fc59d4ffad73b992d3ae
Simone Rosani <rsn.smn@gmail.com>
2024-07-15 14:54:59 +0200

    feat: add maps setup boilerplate

--- app/src/main/java/it/rosani/simplerun/ui/MainActivity.kt
+++ app/src/main/java/it/rosani/simplerun/ui/MainActivity.kt
@@ -1,16 +1,40 @@
 package it.rosani.simplerun.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import it.rosani.simplerun.BuildConfig
+import org.osmdroid.config.Configuration
+import java.io.File
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setupMap()
         enableEdgeToEdge()
         setContent {
             SimpleRunApp()
+        }
+    }
+
+    private fun setupMap() {
+        try {
+            Configuration.getInstance().apply {
+                load(
+                    applicationContext,
+                    applicationContext.getSharedPreferences("osmdroid", MODE_PRIVATE)
+                )
+                userAgentValue = BuildConfig.APPLICATION_ID
+                osmdroidBasePath = applicationContext.filesDir
+                osmdroidTileCache = File(osmdroidBasePath, "tile")
+            }
+            Log.d("MainActivity", "Map setup complete")
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.e("MainActivity", "Error while setting up map", e)
         }
     }
 }
