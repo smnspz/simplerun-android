180f91376ea7ef1b7e292bbe305bb9a818c407b2
Simone Rosani <rsn.smn@gmail.com>
2024-07-15 14:57:20 +0200

    feat: add application navhostcontroller

---
+++ app/src/main/java/it/rosani/simplerun/ui/navigation/SimpleRunNavController.kt
@@ -0,0 +1,36 @@
+package it.rosani.simplerun.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+enum class MainDestinations(val route: String) {
+    HOME("home")
+}
+
+@Composable
+fun rememberSimpleRunNavController(
+    navController: NavHostController = rememberNavController()
+): SimpleRunNavController = remember(navController) {
+    SimpleRunNavController(navController)
+}
+
+class SimpleRunNavController(val navController: NavHostController) {
+    val currentRoute: String?
+        get() = navController.currentDestination?.route
+
+    fun upPress() {
+        navController.navigateUp()
+    }
+
+    fun navigateToRoute(route: String) {
+        if (route != currentRoute) {
+            navController.navigate(route) {
+                launchSingleTop = true
+                restoreState = true
+            }
+        }
+    }
+
+}
