c06e3f374671b9a63564f850b207c8e18021536f
Simone Rosani <rsn.smn@gmail.com>
2024-07-16 21:33:41 +0200

    chore: remove unused imports

--- app/src/main/java/it/rosani/simplerun/ui/components/SimpleRunBottomAppBar.kt
+++ app/src/main/java/it/rosani/simplerun/ui/components/SimpleRunBottomAppBar.kt
@@ -14,23 +14,20 @@ import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import it.rosani.simplerun.ext.toSnakeCase
-import it.rosani.simplerun.ui.main.HomeSections
+import it.rosani.simplerun.models.HomeSections
 
 @Composable
 fun SimpleRunBottomAppBar(
