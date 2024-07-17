0bd3088f2cc3e37551e72354e4658cae1851f9f9
Simone Rosani <rsn.smn@gmail.com>
2024-07-17 17:23:46 +0200

    feat: wrap SimpleRunScaffold with Surface feat: add or remove BottomBar and TopBar programmatically based on current route

--- app/src/main/java/it/rosani/simplerun/ui/components/SimpleRunScaffold.kt
+++ app/src/main/java/it/rosani/simplerun/ui/components/SimpleRunScaffold.kt
@@ -1,29 +1,52 @@
 package it.rosani.simplerun.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import it.rosani.simplerun.models.HomeSections
+import it.rosani.simplerun.ui.main.MainBottomAppBar
+import it.rosani.simplerun.ui.main.MainTopAppBar
 
 @Composable
 fun SimpleRunScaffold(
     modifier: Modifier = Modifier,
     currentRoute: HomeSections,
     navigateToRoute: (String) -> Unit,
+    upPress: () -> Unit,
     content: @Composable (PaddingValues) -> Unit
 ) {
-    Scaffold(
-        modifier = modifier,
-        bottomBar = {
-            SimpleRunBottomAppBar(
-                tabs = HomeSections.entries.toTypedArray(),
-                currentRoute = currentRoute.route,
-                navigateToRoute = navigateToRoute
-            )
-        },
-        content = { padding ->
-            content(padding)
-        }
-    )
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+    ) {
+        Scaffold(
+            modifier = modifier,
+            bottomBar = {
+                if (currentRoute != HomeSections.MAIN) {
+                    SimpleRunBottomAppBar(
+                        tabs = HomeSections.entries.toTypedArray(),
+                        currentRoute = currentRoute.route,
+                        navigateToRoute = navigateToRoute
+                    )
+                } else {
+                    MainBottomAppBar()
+                }
+            },
+            topBar = {
+                if (currentRoute == HomeSections.MAIN) {
+                    MainTopAppBar(upPress = upPress)
+                }
+            },
+            content = { padding ->
+                content(padding)
+            }
+        )
+    }
 }
+
