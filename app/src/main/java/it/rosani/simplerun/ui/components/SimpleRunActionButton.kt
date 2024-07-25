d0426824edeca5c3f141fb6f685f0b8c39e172c2
Simone Rosani <rsn.smn@gmail.com>
2024-07-25 12:08:04 +0200

    chore: refactor StartButton to be more generic

--- app/src/main/java/it/rosani/simplerun/ui/components/SimpleRunActionButton.kt
+++ app/src/main/java/it/rosani/simplerun/ui/components/SimpleRunActionButton.kt
@@ -1,5 +1,6 @@
 package it.rosani.simplerun.ui.components
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
@@ -8,15 +9,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun StartButton(
+fun SimpleRunActionButton(
     modifier: Modifier = Modifier,
     circleColor: Color = Color.Black,
-    textColor: Color = Color.White
+    textColor: Color = Color.White,
+    @StringRes text: Int 
 ) {
     Box(
         modifier = modifier.size(80.dp), // Adjust size as needed
@@ -29,7 +32,7 @@ fun StartButton(
             )
         }
         Text(
-            text = "START",
+            text = stringResource(text),
             color = textColor,
             fontSize = 16.sp,
             fontWeight = FontWeight.Bold
